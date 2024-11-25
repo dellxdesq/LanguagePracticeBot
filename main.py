a482524fe5876a2d59550f5172f2f00329113655
dellxdesq <nikita673551@gmail.com>
2024-11-25 22:07:31 +0300

    фикс много чего

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from ollama_handler import OllamaAI
+import asyncio
+from dotenv import dotenv_values
 from bot import TelegramBot
 
 if __name__ == "__main__":
     bot_token = ""
     bot = TelegramBot(bot_token)
     bot.run()
+    # Загрузка переменных из файла config.env
+    config = dotenv_values("config.env")
 
+    # Получение токена
+    bot_token = config.get("BOT_TOKEN")
+
+    bot = TelegramBot(bot_token)
+
+    asyncio.run(bot.run())  # Теперь вызываем метод run для запуска бота
