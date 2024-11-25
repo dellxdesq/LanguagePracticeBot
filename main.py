b49c54d42412b5bef3647f7dd90a58b26861f7a9
Nikita Marchenko <117754477+dellxdesq@users.noreply.github.com>
2024-11-25 13:18:33 +0300

    a

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from ollama_handler import OllamaAI
 from bot import TelegramBot
 
 if __name__ == "__main__":
-    bot_token = "7645375910:AAF1eaxwLsTFPHlqdPMOg-A-Oof_B1H34Cc"
+    bot_token = ""
     bot = TelegramBot(bot_token)
     bot.run()
 
